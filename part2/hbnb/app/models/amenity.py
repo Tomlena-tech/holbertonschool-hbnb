546ecb3aadbf5a6ffc071b08cc4dc98571ca27cf
Tomlena-tech <decourttthomas@gmail.com>
2025-10-14 19:23:45 +0200

    Add new amenities.py

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Amenity model"""
-from models.base_model import BaseModel
+from app.models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
@@ -22,3 +22,4 @@ class Amenity(BaseModel):
             raise ValueError("Name is required and must not exceed 50 characters")
         
         self.name = name
+        
