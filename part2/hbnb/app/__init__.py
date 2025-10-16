545f0b3cc37dd88d205436127086fb20dcafe4f8
Tomlena-tech <decourttthomas@gmail.com>
2025-10-16 12:51:42 +0200

    init.py back to exemple

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,14 +1,11 @@
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
 
-app = Flask(__name__)
-api = Api(app, title='HBnB API', version='1.0')
-api.add_namespace(users_ns, path='/api/users')
+def create_app():
+    app = Flask(__name__)
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
-# ---------- AMENITIES ----------
-from app.api.v1.amenities import api as amenities_ns
-api.add_namespace(amenities_ns, path='/api/amenities')
+    # Placeholder for API namespaces (endpoints will be added later)
+    # Additional namespaces for places, reviews, and amenities will be added later
+
+    return app
