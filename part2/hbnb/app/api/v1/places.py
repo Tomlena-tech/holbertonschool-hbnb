ddeb985bdd8bc5664c851bb5051b0a5215e17809
Tomlena-tech <decourttthomas@gmail.com>
2025-10-17 00:16:48 +0200

    double place_model delete

--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -29,17 +29,7 @@ review_model = api.model('PlaceReview', {
     'rating': fields.Integer(description='Rating given in the review'),
     'user': fields.Nested(user_model, description='User who wrote the review')
 })
-place_model = api.model('Place', {
-    'id': fields.String(description='Place ID'),
-    'title': fields.String(required=True, description='Title of the place'),
-    'description': fields.String(description='Description of the place'),
-    'price': fields.Float(required=True, description='Price per night'),
-    'latitude': fields.Float(required=True, description='Latitude of the place'),
-    'longitude': fields.Float(required=True, description='Longitude of the place'),
-    'owner': fields.Nested(user_model, description='Owner of the place'),
-    'amenities': fields.List(fields.Nested(amenity_model), description='List of amenities'),
-    'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
-})
+
 @api.route('/')
 class PlaceList(Resource):
     @api.expect(place_model, validate=True)
