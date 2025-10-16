b64d5c9a4c05cc72fb3aedd8c91fed9e61e3e143
Tomlena-tech <decourttthomas@gmail.com>
2025-10-17 00:25:26 +0200

    facade settings

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -1,5 +1,5 @@
 from flask_restx import Namespace, Resource, fields
-from app.services.facade import facade
+from app.services import facade
 
 api = Namespace('amenities', description='Amenity operations')
 
