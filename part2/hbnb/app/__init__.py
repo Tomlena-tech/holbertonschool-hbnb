3a398eac34ef92fe7083d40cbc1c7f611f1c309a
Tomlena-tech <decourttthomas@gmail.com>
2025-10-22 20:53:23 +0200

    Add into app__init__.py "    app.config.from_object(config_class)"

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -7,6 +7,7 @@ from app.api.v1.reviews import api as reviews_ns
 
 def create_app(config_class='config.DevelopmentConfig'):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Register the users namespace
