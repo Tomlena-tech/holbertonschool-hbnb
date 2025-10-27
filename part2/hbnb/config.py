26af04c27a7bd3c3e2b57262852af21c65e23909
Tomlena-tech <decourttthomas@gmail.com>
2025-10-27 12:55:39 +0100

    Add into config.py SQLALchemt + JWT

--- part2/hbnb/config.py
+++ part2/hbnb/config.py
@@ -1,13 +1,19 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'hbnb.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret')
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///hbnb_dev.db'
+
 
 
 config = {
