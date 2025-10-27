693a0ce6455a52a82c9ba438d487f334203412f9
Tomlena-tech <decourttthomas@gmail.com>
2025-10-27 12:51:17 +0100

    Add on init app db(orm) & jwt app

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -8,6 +14,9 @@ from app.api.v1.reviews import api as reviews_ns
 def create_app(config_class='config.DevelopmentConfig'):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    db.init_app(app)
+    jwt.init_app(app)
+    
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Register the users namespace
@@ -18,4 +27,9 @@ def create_app(config_class='config.DevelopmentConfig'):
     api.add_namespace(places_ns, path='/api/v1/places')
     # Register the reviews namespace
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    
+    
+    with app.app_context():
+        db.create_all()
+    
     return app
