32e92738758e2b171e81dd54a2a86197f8528c0c
Tomlena-tech <decourttthomas@gmail.com>
2025-10-29 11:30:09 +0100

    update init.py with auth / update testapi

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -12,6 +12,7 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns 
 
 def create_app(config_class='config.DevelopmentConfig'):
     app = Flask(__name__)
@@ -42,6 +43,8 @@ def create_app(config_class='config.DevelopmentConfig'):
     api.add_namespace(places_ns, path='/api/v1/places')
     # Register the reviews namespace
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    # Register the auth namespace
+    api.add_namespace(auth_ns, path='/api/v1/auth') 
     
     
     with app.app_context():
