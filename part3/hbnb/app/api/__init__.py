0e0c429995a113ddc7e467b511f7a3fbd760e95e
Tomlena-tech <decourttthomas@gmail.com>
2025-11-01 07:52:00 +0100

    update part 3

--- part3/hbnb/app/api/__init__.py
+++ part3/hbnb/app/api/__init__.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from config import Config
 from app.api.v1 import api_v1   # ← notre API unique
-from app.services import facade
-from app.models.user import User
 
 db = SQLAlchemy()
 jwt = JWTManager()
@@ -17,11 +15,4 @@ def create_app(config_class=Config):
     jwt.init_app(app)
     api_v1.init_app(app)         # ← branchement unique
 
-    
-    if not facade.get_user_by_email("admin@hbnb.com"):
-        admin = User("Admin", "User", "admin@hbnb.com", "root123", True)
-        facade.user_repo.add(admin)
-        print("✅ Admin initialisé automatiquement :", admin.to_dict())
-    
     return app
-
